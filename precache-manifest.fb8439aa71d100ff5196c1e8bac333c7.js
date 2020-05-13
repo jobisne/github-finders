db1902b6b1a50bb809ef24ddccabfdddc4dda5d8
jobi <olajobinurudeen2013@gmail.com>
2020-05-13 16:03:01 +0100

    Updates

---
+++ precache-manifest.fb8439aa71d100ff5196c1e8bac333c7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "989dd46e2928eda119faee26327040ad",
+    "url": "/github-finders/index.html"
+  },
+  {
+    "revision": "aab16b0d35f10caada72",
+    "url": "/github-finders/static/css/main.f65227a3.chunk.css"
+  },
+  {
+    "revision": "05d8a6e621b85f3c2785",
+    "url": "/github-finders/static/js/2.0189716e.chunk.js"
+  },
+  {
+    "revision": "aab16b0d35f10caada72",
+    "url": "/github-finders/static/js/main.f2a7ccab.chunk.js"
+  },
+  {
+    "revision": "e49ee9f95e02ffe341a6",
+    "url": "/github-finders/static/js/runtime-main.6d274e60.js"
+  }
+]);
